8da8fab34c61a3f240c6efa4fb26b370c6759df6
Praved <praved.official@gmail.com>
2022-02-17 14:15:00 +0530

    first commit

---
+++ inotebook-backend/db.js
@@ -0,0 +1,12 @@
+const mongoose =require('mongoose');
+
+const mongooseURL="mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+
+const connectToMongo=()=>{
+
+    mongoose.connect(mongooseURL,()=>{
+        console.log("Database Connection Successful");
+    })
+}
+
+module.exports =connectToMongo;
